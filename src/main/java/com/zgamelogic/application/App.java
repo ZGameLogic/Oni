5967edf5971a8f3ac8f88c44247a2649b41c945c
Ben Shabowski <benshabowski@gmail.com>
2026-03-19 09:40:11 -0500

    fix: remove author comment from App.java and update GitHub repository URL in pom.xml

--- src/main/java/com/zgamelogic/application/App.java
+++ src/main/java/com/zgamelogic/application/App.java
@@ -3,9 +3,6 @@ package com.zgamelogic.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author Ben Shabowski
- */
 @SpringBootApplication
 public class App {
     public static void main(String[] args){
